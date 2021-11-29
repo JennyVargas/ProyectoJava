cf127cf636667325a053775c0429c95d871a6d8f
laesp <laesp@LAPTOP-BG846JL2>
2021-11-28 22:09:21 -0300

    agregue parte registro

--- src/controlador/Registro.java
+++ src/controlador/Registro.java
@@ -4,6 +4,8 @@ import bd.conexion;
 import java.util.Date;
 import modelo.Datoscita;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
 /**
  *
  * @author jenny
@@ -21,11 +23,73 @@ public class Registro {
             
             date = datoscita.getFecha();
             
-            String query = "dd/mm/yyyy";
-            
-            
-        }catch (Exception e){
+             String query = "INSERT INTO datoscita(rut,nombre,apellido,telefono,edad,prevision,doctor,especialidad,fecha,valor) VALUES (?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, datoscita.getRut());
+            stmt.setString(2, datoscita.getNombre());
+            stmt.setString(3, datoscita.getApellido());
+            stmt.setInt(4, datoscita.getTelefono());
+            stmt.setInt(5, datoscita.getEdad());
+            stmt.setString(6, datoscita.getPrevision());
+            stmt.setString(7, datoscita.getDoctor());
+            stmt.setString(8, datoscita.getEspecialidad());
+            stmt.setDate(9, new java.sql.Date(date.getTime()));
+            stmt.setInt(10, datoscita.getValor());
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error SQL al agregar Rut: " + e.getMessage());
+            return false;
+        } catch(Exception e){
+            System.out.println("Error al agregar Rut (EXCEPTION): " + e.getMessage());
+            return false;
         }
-        return false;
     }
- }
+
+    public boolean actualizar (Datoscita datoscita){
+        
+        
+        
+        try {
+            conexion con = new conexion();
+            Connection cnx = con.obtenerConexion();
+            
+           
+            
+             String query = "UPDATE libro set datoscita = ?,rut = ?, nombre = ?, apellido = ?, telefono = ?, edad = ?,prevision = ?, doctor = ?, especialidad = ?, fecha = ?, valor = ?  WHERE rut = ?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, datoscita.getRut());
+            stmt.setString(2, datoscita.getNombre());
+            stmt.setString(3, datoscita.getApellido());
+            stmt.setInt(4, datoscita.getTelefono());
+            stmt.setInt(5, datoscita.getEdad());
+            stmt.setString(6, datoscita.getPrevision());
+            stmt.setString(7, datoscita.getDoctor());
+            stmt.setString(8, datoscita.getEspecialidad());
+            stmt.setDate(9, new java.sql.Date(datoscita.getFecha().getTime()));
+            stmt.setInt(10, datoscita.getValor());
+
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+
+            return true;
+
+        } catch (SQLException e) {
+            System.out.println("Error SQL al agregar Rut: " + e.getMessage());
+            return false;
+        } catch(Exception e){
+            System.out.println("Error al agregar Rut (EXCEPTION): " + e.getMessage());
+            return false;
+        }
+    }
+
+    
+
+
+      
